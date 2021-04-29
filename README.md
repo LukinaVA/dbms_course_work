2aadcbfc33f4d02bd3d5465cbc4a78bc10dfc484
LukinaVA <lukina.va@edu.spbstu.ru>
2021-04-29 08:52:54 +0300

    Init server

---
+++ README.md
@@ -0,0 +1,16 @@
+# DBMS Course Work
+
+Automation of the hospital's emergency department.
+
+## Technology stack
+Server
+* Node.js
+* Express
+* Oracle
+
+Client
+* React JS
+
+## How to get started
+cd server && npm i && npm start  
+cd client && npm i && npm start
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const webServer = require('./services/web-server.js');
+
+async function startup() {
+    console.log('Starting application');
+
+    try {
+        console.log('Initializing web server module');
+
+        await webServer.initialize();
+    } catch (err) {
+        console.error(err);
+
+        process.exit(1); // Non-zero failure code
+    }
+}
+
+startup();
