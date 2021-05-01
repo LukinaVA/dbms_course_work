51a08d9adf54b0a617cdaa0c41dd708fb69e0bf8
LukinaVA <lukina.va@edu.spbstu.ru>
2021-05-01 18:07:49 +0300

    Added routing, controller, and database logic to handle the HTTP GET request

---
+++ server/db_apis/people.js
@@ -0,0 +1,21 @@
+const database = require('../services/database.js');
+
+const baseQuery =
+    `select * from people`;
+
+async function find(context) {
+    let query = baseQuery;
+    const binds = {};
+
+    if (context.id) {
+        binds.id = context.id;
+
+        query += `\nwhere id = :id`;
+    }
+
+    const result = await database.simpleExecute(query, binds);
+
+    return result.rows;
+}
+
+module.exports.find = find;
